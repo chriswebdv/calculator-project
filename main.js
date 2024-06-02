6c1d8282d5b52968c9382fbb9fd001ffbec1847a
chriswebdv <chriswebdv@gmail.com>
2024-06-02 15:22:11 -0600

    Use query selector to select html items

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ function multNums(num1, num2) {
 function divideNums(num1, num2) {
   return num1 / num2;
 }
+
+let screenView = document.querySelector(".screen-view");
+let btn = document.querySelectorAll("button");
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", function () {
+    if (btn[i] === "+") {
+      screenView.textContent = addNums(num1, num2);
+    }
+  });
+}
