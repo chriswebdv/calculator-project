0685c4076e6cffe2174994731b2df8aaed87df84
chriswebdv <chriswebdv@gmail.com>
2024-06-02 14:08:40 -0600

    Add html to create buttons and screen of the calculator

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 let num1;
 let num2;
 
-let operator;
-
 function addNums(num1, num2) {
   return num1 + num2;
 }
@@ -18,8 +16,3 @@ function multNums(num1, num2) {
 function divideNums(num1, num2) {
   return num1 / num2;
 }
-
-console.log(addNums(10, 5));
-console.log(subtractNums(10, 5));
-console.log(multNums(10, 5));
-console.log(divideNums(10, 5));
