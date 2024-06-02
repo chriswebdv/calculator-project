3ea97a689877dabf3064cff7a1e2bbe234f11bf8
chriswebdv <chriswebdv@gmail.com>
2024-06-02 13:30:21 -0600

    Creat the 4 basic arithmetic functions and added each to a function

---
+++ main.js
@@ -0,0 +1,25 @@
+let num1;
+let num2;
+
+let operator;
+
+function addNums(num1, num2) {
+  return num1 + num2;
+}
+
+function subtractNums(num1, num2) {
+  return num1 - num2;
+}
+
+function multNums(num1, num2) {
+  return num1 * num2;
+}
+
+function divideNums(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(addNums(10, 5));
+console.log(subtractNums(10, 5));
+console.log(multNums(10, 5));
+console.log(divideNums(10, 5));
