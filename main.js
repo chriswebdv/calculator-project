18a6a74c600c3dd50a69f57dcaec905404d17551
chriswebdv <chriswebdv@gmail.com>
2024-06-03 17:51:21 -0600

    Refarctored for each loop to include event listeners for each button of the calculator

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let num1 = 10;
-let num2 = 2;
+let num1;
+let num2;
 
 function addNums(num1, num2) {
   return num1 + num2;
@@ -20,8 +20,9 @@ function divideNums(num1, num2) {
 let screenView = document.querySelector(".screen-view");
 let btns = document.querySelectorAll("button");
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    screenView.textContent = num1 + num2;
+btns.forEach((bt) => {
+  bt.addEventListener("click", (e) => {
+    screenView = screenView.textContent = e.target.innerHTML;
+    console.log(screenView);
   });
 });
